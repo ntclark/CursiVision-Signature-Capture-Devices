64e8b72591fa34b48f9a2524a757481a2b9f415c
Nate <nate@useclark.com>
2019-06-15 13:27:16 -0400

    Adding Wacom sdk sources

---
+++ Wacom/Generation 2/Wacom STU SDK/cpp/include/WacomGSS/config/win32.hpp
@@ -0,0 +1,12 @@
+/// @file      WacomGSS/config/win32.hpp
+/// @copyright Copyright (c) 2011 Wacom Company Limited
+/// @author    mholden
+/// @date      2011-10-18
+/// @brief     meta configuration about the compliation platform and compiler
+
+#ifndef WacomGSS_config_win32_hpp
+#define WacomGSS_config_win32_hpp
+
+#define WacomGSS_WIN32 1
+
+#endif // WacomGSS_config_win32_hpp
